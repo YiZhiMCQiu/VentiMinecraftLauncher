f03dd1d50b070b53617f91de08da776ae38ed5e0
YiZhiMCQiu <pikachu20130124@163.com>
2025-03-15 13:24:49 +0800

    Initial Commit

---
+++ VentiMinecraftLauncher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VentiMinecraftLauncher
+//
+//  Created by YiZhiMCQiu on 2025/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VentiMinecraftLauncher/VentiMinecraftLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  VentiMinecraftLauncherApp.swift
+//  VentiMinecraftLauncher
+//
+//  Created by YiZhiMCQiu on 2025/3/15.
+//
+
+import SwiftUI
+
+@main
+struct VentiMinecraftLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
